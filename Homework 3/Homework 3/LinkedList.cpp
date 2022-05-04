3a3a4ec647a40a446190cc7d3db9736ad0f31603
petr112gusakov <90793757+petr112gusakov@users.noreply.github.com>
2022-05-04 19:34:13 +0300

    Add files via upload

---
+++ Homework 3/Homework 3/LinkedList.cpp
@@ -0,0 +1,170 @@
+
+#include "LinkedList.h"
+
+std::ostream& operator<<(std::ostream& stream, const Node& node)
+{
+    stream << node.data;
+    return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, const LinkedList& list)
+{
+    stream << "[" << list.count << "] : { ";
+    Node* temp = list.head;
+    while (temp != nullptr)
+    {
+        stream << *temp << " ";
+        //stream << temp->data << " ";
+        temp = temp->next;
+    }
+    stream << "}";
+    return stream;
+}
+
+LinkedList::~LinkedList()
+{
+    while (head != nullptr)
+    {
+        popHead();
+    }
+}
+
+void LinkedList::pushHead(int element)
+{
+    /*
+    Node* newhead = new Node();
+    newhead->data = element;
+    newhead->next = this->head;
+    head = newhead;
+    if (tail == nullptr)
+    {
+        tail = head;
+    }
+    ++count;*/
+    head = new Node(element, head);
+    if (tail == nullptr)
+    {
+        tail = head;
+    }
+    ++count;
+}
+
+void LinkedList::pushTail(int element)
+{
+    if (tail == nullptr)
+    {
+        return pushHead(element);
+    }
+    tail->next = new Node(element);
+    tail = tail->next;
+    ++count;
+}
+
+void LinkedList::insert(int element, int index)
+{
+    if (index == 0)
+    {
+        return pushHead(element);
+    }
+    if (index == count)
+    {
+        return pushTail(element);
+    }
+
+    int n = 0;
+    Node* temp = head;
+    while (n < index - 1)
+    {
+        temp = temp->next;
+        ++n;
+    }
+    temp->next = new Node(element, temp->next);
+    ++count;
+}
+
+int LinkedList::extract(int index)
+{
+    if (index == 0)
+    {
+        return popHead();
+    }
+    if (index == count)
+    {
+        return popTail();
+    }
+
+    int n = 0;
+    Node* temp = head;
+    while (n < index - 1)
+    {
+        temp = temp->next;
+        ++n;
+    }
+    Node* extr = temp->next;
+    int value = extr->data;
+    temp->next = extr->next;
+    delete extr;
+    --count;
+    return value;
+}
+
+void LinkedList::swap(int index1, int index2)
+{
+    Node* temp1 = this->head;
+    for (int i = 0; i < index1; ++i)
+    {
+        temp1 = temp1->next;
+    }
+    Node* temp2 = this->head;
+    for (int i = 0; i < index2; ++i)
+    {
+        temp2 = temp2->next;
+    }
+
+    int t = temp1->data;
+    temp1->data = temp2->data;
+    temp2->data = t;
+}
+
+int LinkedList::popHead()
+{
+    if (head == nullptr)
+    {
+        return 0;
+    }
+    --count;
+    Node* temp = head;
+    int value = head->data;
+    head = head->next;
+    delete temp;
+    if (head == nullptr)
+    {
+        tail = nullptr;
+    }
+    return value;
+}
+
+int LinkedList::popTail()
+{
+    if (tail == nullptr)
+    {
+        return 0;
+    }
+    if (head == tail)
+    {
+        return popHead();
+    }
+    Node* temp = head;
+    while (temp->next != tail)
+    {
+        temp = temp->next;
+    }
+    --count;
+    int value = tail->data;
+    delete tail;
+    tail = temp;
+    tail->next = nullptr;
+    return value;
+
+
+}
